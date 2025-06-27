5d9674016f1d039afa7b724599e8c3e91b35a7e7
Maycol88 <maycol.amaral2014@gmail.com>
2025-06-27 01:10:30 -0300

    Atualizando API_URL e corrigindo CORS

--- src/app/api.js
+++ src/app/api.js
@@ -1,14 +1,20 @@
 // src/api.js
-export const API_URL = 'http://100.109.226.27:8000';
+export const API_URL = 'https://gc-back.onrender.com';
+
 export const API_ENDPOINTS = {
-  listarUsuarios: `${API_URL}/api/usuarios/listar_users.php`,
-  cadastrarUsuario: `${API_URL}/api/usuarios/cadastrar_user.php`,
-  editarUsuario: `${API_URL}/api/usuarios/editar_user.php`,
-  excluirUsuario: `${API_URL}/api/usuarios/excluir_user.php`,
-  vincularColaborador: `${API_URL}/api/colaboradores/vincular_colaborador.php`,
-  cadastrarUnidade: `${API_URL}/api/unidades/cadastrar_unidade.php`,
-  listarUnidades: `${API_URL}/api/unidades/listar_unidades.php`,
-  carregarEscala: `${API_URL}/api/escala/carregar_escala.php`,
-  salvarEscala: `${API_URL}/api/escala/salvar_escala.php`,
-  gerarRelatorioMensal: `${API_URL}/api/relatorios/gerar_relatorio_mensal.php`
-};  
+  listarUsuarios: `${API_URL}/usuarios/listar_users.php`,
+  cadastrarUsuario: `${API_URL}/usuarios/cadastrar_user.php`,
+  editarUsuario: `${API_URL}/usuarios/editar_user.php`,
+  excluirUsuario: `${API_URL}/usuarios/excluir_user.php`,
+  vincularColaborador: `${API_URL}/colaboradores/vincular_colaborador.php`,
+  cadastrarUnidade: `${API_URL}/unidades/cadastrar_unidade.php`,
+  listarUnidades: `${API_URL}/unidades/listar_unidades.php`,
+  carregarEscala: `${API_URL}/escala/carregar_escala.php`,
+  salvarEscala: `${API_URL}/escala/salvar_escala.php`,
+  gerarRelatorioMensal: `${API_URL}/relatorios/gerar_relatorio_mensal.php`
+};
+export const API_HEADERS = {
+  'Content-Type': 'application/json',
+  'Accept': 'application/json',
+  'Access-Control-Allow-Origin': '*'
+};
